751bb5d5d0cb14edfc7aea3609dc606aa656f658
Meenu Venkittarajan <vpmeenu@gmail.com>
2016-02-24 11:49:11 -0500

    Adding the magpie call to retrieve json

--- web/js/messageController.js
+++ web/js/messageController.js
@@ -4,6 +4,11 @@ angular.module('personalWebsite.controllers').controller('messageController', ['
   $scope.names = ["Magpie", "Meenu", "Alice"];
   $scope.sendMessage = function() {
       $scope.messages.push($scope.currentMessage);
+      var xmlHttp = new XMLHttpRequest();
+      var magpie_url = "http://localhost:5000/website?src=" + $scope.currentMessage;
+      xmlHttp.open( "GET", magpie_url, false ); // false for synchronous request
+      xmlHttp.send( null );
       $scope.currentMessage = "";
+      $scope.messages.push(xmlHttp.responseText);
   }
 }]);
