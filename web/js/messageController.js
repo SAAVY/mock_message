0e3e4967a4db7cb2db4f171db68016fefbec0bc1
Meenu Venkittarajan <vpmeenu@gmail.com>
2016-03-06 23:25:05 -0500

    Major formatting changes + async api call

--- web/js/messageController.js
+++ web/js/messageController.js
@@ -1,16 +1,17 @@
-angular.module('personalWebsite.controllers').controller('messageController', ['$scope', '$location', '$sce',  function($scope, $location, $sce) {
+angular.module('personalWebsite.controllers').controller('messageController', ['$scope', '$location', '$sce', '$http',  function($scope, $location, $sce, $http) {
     $scope.messages = [];
     $scope.currentMessage = "";
     $scope.htmlContent = "";
     $scope.names = ["Magpie", "Meenu", "Alice"];
     $scope.sendMessage = function() {
-        $scope.htmlContent = $sce.trustAsHtml($scope.currentMessage);
+        var messageBubble = "<div id=\"message_bubble\">" + $scope.currentMessage + "</div>"
+        $scope.htmlContent = $sce.trustAsHtml(messageBubble);
         $scope.messages.push($scope.htmlContent);
         var urlArray = [];
         var matchArray;
 
         // Regular expression to find FTP, HTTP(S) and email URLs.
-        var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g;
+        var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)/g;
 
         // Iterate through any URLs in the text.
         while( (matchArray = regexToken.exec( $scope.currentMessage )) !== null ) {
@@ -19,18 +20,23 @@ angular.module('personalWebsite.controllers').controller('messageController', ['
         }
 
         for (var i = 0; i < urlArray.length; i++) {
-            var xmlHttp = new XMLHttpRequest();
-            var magpie_url = "http://localhost:5000/website?src=" + urlArray[i];
-            xmlHttp.open("GET", magpie_url, false); // false for synchronous request
-            xmlHttp.send(null);
-            var card = $scope.createCard(xmlHttp.responseText);
-            $scope.messages.push($sce.trustAsHtml(card));
+            $http({
+                method: 'GET',
+                url: 'http://localhost:5000/website?src=' + urlArray[i] + '&desc_cap=100'
+            }).then(function successCallback(response) {
+                console.log(response);
+                var card = $scope.createCard(response.data);
+                $scope.messages.push($sce.trustAsHtml(card));
+            }, function errorCallback(response) {
+                alert(response);
+            });
         }
         $scope.currentMessage = "";
     }
     $scope.createCard = function(json) {
         var cardHtml = "";
-        var validJson = JSON.parse(json);
+        console.log(json);
+        var validJson = json;
 
         if (validJson['data']['media'] != null && validJson['data']['media']['data'] != null && validJson['data']['media']['data'][0]['iframe'] != null) {
             cardHtml += "<div class=\"card has-media\">";
@@ -44,35 +50,34 @@ angular.module('personalWebsite.controllers').controller('messageController', ['
             cardHtml += "<img class=\"favicon\" src=\"" +  validJson['data']['favicon'] + "\"/>";
         }
         if (validJson['data']['provider_url'] != null) {
-            cardHtml += "<div class=\"website-name\"> <p>" + validJson['data']['provider_url'] + "</p></div>";
+            cardHtml += "<span>" + validJson['data']['provider_url'] + "</span>";
         }
         cardHtml += "</div>" //Header div close
 
-        cardHtml += "<div class=\"left-col\">";
+        cardHtml += "<div class=\"top-div\">";
         if (validJson['data']['media'] != null
         && validJson['data']['media']['data'] != null
         && validJson['data']['media']['data'][0]['iframe'] != null) {
             cardHtml += "<div class=\"media\">" + validJson['data']['media']['data'][0]['iframe'] + "</div>";
         } else if (validJson['data']['images'] != null && validJson['data']['images']['data'] != null) {
             cardHtml += "<div class=\"images\">";
-            cardHtml += "<img class=\"image\" src=\"" + validJson['data']['images']['data'][0]['url'] + "></img>";
+            cardHtml += "<img class=\"image\" src=\"" + validJson['data']['images']['data'][0]['url'] + "\"></img>";
             cardHtml += "</div>";
         }
         cardHtml += "</div>"; //Closes Left Col Div
 
-        cardHtml += "<div class=\"right-col\">";
+        cardHtml += "<div class=\"bottom-div\">";
         if (validJson['data']['title'] != null) {
-            cardHtml += "<h2 class=\"title\">" + validJson['data']['title'] + "</h2>";
+            if (validJson['data']['provider_url'] != null && validJson['data']['url'] != null) {
+                cardHtml += "<h5 class=\"title\"><a href=\"" + validJson['data']['url'] + "\"><strong>" + validJson['data']['title'] + "</strong></a></h5>";
+            } else {
+                cardHtml += "<h5 class=\"title\"><strong>" + validJson['data']['title'] + "</strong></h5>";
+            }
         }
         if (validJson['data']['description'] != null) {
             cardHtml += "<p class=\"desc\">" + validJson['data']['description'] + "</p>";
         }
-        if (validJson['data']['provider_url'] != null && validJson['data']['url'] != null) {
-            cardHtml += "<div class=\"read_more\"><a href=\"" + validJson['data']['url'] + "\"> See more on " + validJson['data']['provider_url'] + "</a>";
-            cardHtml += "</div>";
-        }
         cardHtml += "</div></div>";
-
         return cardHtml;
     }
 }]);
