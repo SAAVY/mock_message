b30115d3a12b8fdc67ebb905f87ccc1960ec8fde
Meenu Venkittarajan <vpmeenu@gmail.com>
2016-03-08 22:51:46 -0500

    CSS changes

--- web/js/messageController.js
+++ web/js/messageController.js
@@ -65,7 +65,7 @@ angular.module('magpieDemo.controllers').controller('messageController', ['$scop
         } else if (validJson['data']['images'] != null && validJson['data']['images']['data'] != null) {
             cardHtml += "<div class=\"images\">";
             cardHtml += "<img class=\"image\" src=\"" + validJson['data']['images']['data'][0]['url'] + "\" width=\"100%\"></img>";
-            cardHtml += "</div>";
+            cardHtml += "</div>";            
         }
         cardHtml += "</div>"; //Closes Left Col Div
 
